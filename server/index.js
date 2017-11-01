582994f09398122481d823b08fb7a502a14c0127
Max Gavrosh <mmg@anadeainc.com>
2017-11-01 15:58:15 +0200

    Update index

--- server/index.js
+++ server/index.js
@@ -1,6 +1,31 @@
 import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import bodyParser from 'body-parser';
+import { createServer } from 'http';
+import { Schema } from './data/schema';
+import { Mocks } from './data/mocks';
 
-const PORT = 8080;
+const GRAPHQL_PORT = 8080;
 const app = express();
+const executableSchema = makeExecutableSchema({
+  typeDefs: Schema,
+});
 
-app.listen(PORT, () => console.log(`Server is now running on http://localhost:${PORT}`));
+addMockFunctionsToSchema({
+  schema: executableSchema,
+  mocks: Mocks,
+  preserveResolvers: true,
+});
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema: executableSchema,
+  context: {},
+}));
+
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
+
+const graphQLServer = createServer(app);
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(`GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`));
